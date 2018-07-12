b961dd044038238a2d0594d571e588057a07ef1d
Carp1224 <41133628+Carp1224@users.noreply.github.com>
2018-07-12 11:30:18 +0800

    ok

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿#include "mainwindow.h"
+#include "odbchelper.h"
+#include <QApplication>
+#include "vtkConeSource.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    odbchelper k;
+    k.getpointid();
+    return a.exec();
+}
---
+++ odbchelper.h
@@ -0,0 +1,15 @@
+﻿#ifndef ODBCHELPER_H
+#define ODBCHELPER_H
+#include "QtSql/qsqlquery.h"
+#include "QtSql/qsqldatabase.h"
+#include "QtSql/qsqlerror.h"
+#include <QDebug>
+#include <QMessageBox>
+
+class odbchelper{
+public:
+    odbchelper();
+    void getpointid();
+    QSqlDatabase db;
+};
+#endif // ODBCHELPER_H

e6114526acca756b835f3dac999415168b910283
Carp1224 <1220575318@qq.com>
2018-07-13 10:49:08 +0800

    emm

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,17 @@
+﻿#include <vtkAutoInit.h>
+VTK_MODULE_INIT(vtkRenderingOpenGL2)
+VTK_MODULE_INIT(vtkInteractionStyle)
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QVTKWidget.h>
+#include <vtkRenderer.h>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QVTKWidget *a=new QVTKWidget(this);
+    this->setCentralWidget(a);
 }
 
 MainWindow::~MainWindow()
